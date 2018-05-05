ccd2c248ce8118c29fd6557306fbf29da798135f
Ryan <1624600@student.fife.ac.uk>
2018-05-05 15:38:17 +0100

    a

--- app/core/App.php
+++ app/core/App.php
@@ -31,7 +31,7 @@ class App{
 
         $this->params = $url ? array_values($url) : [];
 
-        call_user_func_array([$this->controller, $this-method], $this->params); 
+        call_user_func_array([$this->controller, $this->method], $this->params); 
      }
     }
 
