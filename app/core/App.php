c7d306663d027cac84e19ecdf9e94f51313bb422
Ryan <1624600@student.fife.ac.uk>
2018-05-05 15:54:39 +0100

    a

--- app/core/App.php
+++ app/core/App.php
@@ -12,20 +12,21 @@ class App{
      $url = $this->parseUrl();
 
 
-     if(file_exists('../app/controllers/'. $url[1] .'.php')){
-         $this->controller = $url[1];
-         unset($url[1]);
+
+     if(file_exists('../app/controllers/'. $url[0] .'.php')){
+         $this->controller = $url[0];
+         unset($url[0]);
      }
 
      require_once '../app/controllers/' . $this->controller.'.php';
 
      $this->controller = new $this->controller; 
 
-     if(isset($url[2])){
+     if(isset($url[1])){
 
-        if(method_exists($this->controller, $url[2])){
-            $this->method = $url[2];
-            unset($URL[2]);
+        if(method_exists($this->controller, $url[1])){
+            $this->method = $url[1];
+            unset($URL[1]);
         }
 
         $this->params = $url ? array_values($url) : [];
