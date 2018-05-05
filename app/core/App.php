5f634a18c92ad72d9e6e1e5522c3124867512f18
Ryan <1624600@student.fife.ac.uk>
2018-05-05 14:38:50 +0100

    p2

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,20 @@
 <?php
 
 class App{
+
+    protected $controller = 'home';
+
+    protected $method = 'index';
+
+    protected $params = [];
+
     public function __construct(){
-        echo 'okk';
+       
+    }
+
+    public function parseUrl(){
+        if(isset($_GET['url'])){
+            echo $_GET['url'];
+        }
     }
 }
