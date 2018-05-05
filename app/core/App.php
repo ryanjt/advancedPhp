d1b5c73f92498e926d9fc89e76fc2e428c7cffda
Ryan <1624600@student.fife.ac.uk>
2018-05-05 14:48:01 +0100

    a

--- app/core/App.php
+++ app/core/App.php
@@ -9,12 +9,13 @@ class App{
     protected $params = [];
 
     public function __construct(){
-      $this->parseUrl();
+      print_r($this->parseUrl());
     }
 
     public function parseUrl(){
         if(isset($_GET['url'])){
-            echo $_GET['url'];
+            return $url = explode('/',filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL)); 
+
         }
     }
 }
