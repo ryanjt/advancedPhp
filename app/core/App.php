055d0be2087664bc5f4931f0953252b545569956
Ryan <1624600@student.fife.ac.uk>
2018-05-05 15:27:48 +0100

    a

--- app/core/App.php
+++ app/core/App.php
@@ -16,7 +16,7 @@ class App{
          unset($url[0]);
      }
 
-     require_once '..app/controllers/' . $this->controller.'.php';
+     require_once '../app/controllers/' . $this->controller.'.php';
 
      echo $this->controller; 
     }
